d8c2c6721920736815f525541ce6eaeca9a6d0b3
ZienabKhaled <117679026+ZienabKhaled@users.noreply.github.com>
2022-11-17 19:14:43 +0200

    Add files via upload

---
+++ jssss.js
@@ -0,0 +1,10 @@
+var firstName = prompt("Enter your first name");
+
+var lastName = prompt ("Enter your last name "); 
+let confirmName = confirm ( "Your name is " + firstName + " " + lastName );
+ 
+var year = prompt ("Enter your birth year"); 
+
+age = 2022 - year ;
+
+document.write (" Welcome "+ firstName + " " +lastName + " you are "+ age +" years old"); 
---
+++ task2.js
@@ -0,0 +1,9 @@
+alert ( "it’s the first release of a calculator that only has a summation feature.");
+var Number1 = prompt ( " Enter the first number");
+var Number2 = prompt ( " Enter the second number");
+x=Number ( Number1);
+y=Number ( Number2);
+
+Z = x + y ;
+
+document.write ( x + "+" + y + " = " +  Z );
